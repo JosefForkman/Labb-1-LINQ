2975c254d1e3b64ae28d42f56093558c2e2fcf06
Josef Forkman <44498331+JosefForkman@users.noreply.github.com>
2025-04-03 09:47:30 +0200

    add name to Customer

--- Modules/Customer.cs
+++ Modules/Customer.cs
@@ -6,6 +6,7 @@ public class Customer
 {
     public int Id { get; set; }
     [EmailAddress]
+    public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     [Phone]
     public string Phone { get; set; } = string.Empty;
