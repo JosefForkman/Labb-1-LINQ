be46c2047996cac04e0fd4aaae8e3a690eff06b1
Josef Forkman <44498331+JosefForkman@users.noreply.github.com>
2025-04-03 10:20:09 +0200

    add menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Labb_1___LINQ.utils;
+
+
+// Menu
+var menu = new Menu(["exit", "wowo"]);
+
+var curentMenu = menu.Show();
+var menuRuning = true;
+
+while (menuRuning)
+{
+    switch (curentMenu)
+    {
+        case 1:
+            menuRuning = false;
+            break;
+        default:
+            menuRuning = false;
+            break;
+    }
+}
