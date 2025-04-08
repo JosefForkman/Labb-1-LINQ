f7f6d51472566ede02c1e302333a73dbc848c18c
Josef Forkman <44498331+JosefForkman@users.noreply.github.com>
2025-04-08 08:52:23 +0200

    Begining of seed

--- Program.cs
+++ Program.cs
@@ -2,40 +2,42 @@
 using Labb_1___LINQ.Controller;
 using Labb_1___LINQ.utils;
 
+Seed seed = new Seed();
+seed.SeedDataV2();
 
-// Menu
-var menu = new Menu(["Show produkts", "Show supplier with less 10 units", "Get curent order", "Get top 3 produkt", "Get all catogorys", "Get all order ditails", "exit"]);
+// // Menu
+// var menu = new Menu(["Show produkts", "Show supplier with less 10 units", "Get curent order", "Get top 3 produkt", "Get all catogorys", "Get all order ditails", "exit"]);
 
-var menuRuning = true;
+// var menuRuning = true;
 
-while (menuRuning)
-{
-    var curentMenu = menu.Show();
-    switch (curentMenu)
-    {
-        case 0:
-            ProductController.Index("Electronics");
-            break;
-        case 1:
-            SupplierController.Index();
-            break;
-        case 2:
-            OrderController.GetTotalOrderValue();
-            break;
-        case 3:
-            OrderController.GetTopProduct(3);
-            break;
-        case 4:
-            CategoryController.GetCategory();
-            break;
-        case 5:
-            OrderController.GatAllOrdersDitails();
-            break;
-        case 6:
-            menuRuning = false;
-            break;
-        default:
-            menuRuning = false;
-            break;
-    }
-}
+// while (menuRuning)
+// {
+//     var curentMenu = menu.Show();
+//     switch (curentMenu)
+//     {
+//         case 0:
+//             ProductController.Index("Electronics");
+//             break;
+//         case 1:
+//             SupplierController.Index();
+//             break;
+//         case 2:
+//             OrderController.GetTotalOrderValue();
+//             break;
+//         case 3:
+//             OrderController.GetTopProduct(3);
+//             break;
+//         case 4:
+//             CategoryController.GetCategory();
+//             break;
+//         case 5:
+//             OrderController.GatAllOrdersDitails();
+//             break;
+//         case 6:
+//             menuRuning = false;
+//             break;
+//         default:
+//             menuRuning = false;
+//             break;
+//     }
+// }
